6928c33b153270b5dcde7460865e1ce53b1c07c8
brendan <lobrendan@gmail.com>
2022-03-07 14:34:04 -0800

    working on debugging cors problem with doglist

--- src/DogList.js
+++ src/DogList.js
@@ -1,18 +1,37 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-async function DogList() {
-  const [dogs, setDogs] = useState(null);
+function DogList() {
+  const [dogs, setDogs] = useState([]);
 
-  if (dogs === null) {
-    setDogs(await axios.get("localhost:5000/dogs"));
-  }
+  useEffect(() => {
+    axios.get("localhost:5000/dogs").then(data => {
+      setDogs(data)
+    }
+    );
+  });
+
 
   return (
     <div className="DogList">
+      {dogs.map(dog => {
+        return (
+          <div key={dog.name}>
+            <h1>{dog.name}</h1>
+            <p> Age: {dog.age}</p>
+            <p> Facts:
+              <ul>
+                {dog.facts.map(fact => <li>{fact}</li>)}
+              </ul>
+            </p>
 
-    </div>
+            <img src={`${process.env.PUBLIC_URL}/${dog.name}.jpg`} />
+          </div>
+        )
+      })}
+    </div >
   );
 }
+
 
 export default DogList;
